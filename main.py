68bede0d40eb3eca99db058ee2a039236c46f7ee
nikwok <77155696+NIKITAvinogradovs@users.noreply.github.com>
2022-01-25 08:24:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+text = input ("Ievadi tekstu :")
+def countWords(text):
+  summa = 0
+  sar1 = text.split()
+  for word in sar1:
+    if len(word)>1:
+      summa+= 1
+    print (summa)
+  return summa
+countWords(text)
